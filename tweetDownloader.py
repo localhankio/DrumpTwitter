d2471c5ba50e033404dde3a7c1152525acc19c9c
Garrett Beatty <gman9732@gmail.com>
2016-12-28 15:31:15 -0500

    fixed keys

--- tweetDownloader.py
+++ tweetDownloader.py
@@ -1,9 +1,10 @@
 import twitter
+from keys import cons_key, cons_secret, access_sec, access_tok
 
-api = twitter.Api(consumer_key="yo7TOsy8bdIu9ZlK7CdLnd0sN",
-	consumer_secret="sRUFYMk16y1f3jLEsfOfs2S0t0zHDC8vmcaAixH3UVawdXXYFR",
-	access_token_key="800131076622458881-36NJNS5xC5QIZH6BdB4bZIk0pRU902R",
-	access_token_secret="m7apKLNWbTcbHABFbgK40G1w6ysfWAmASPPtp62GqyGVF")
+api = twitter.Api(consumer_key=cons_key,
+	consumer_secret=cons_secret,
+	access_token_key=access_tok,
+	access_token_secret=access_sec)
 
 statuses = api.GetUserTimeline('25073877', count=500)
 
