96204ca54a1dffcfbe7d8b4d29e8695baf1b8ab3
Henry Shen <henry.shen@outlook.com>
2016-12-28 15:27:38 -0500

    Update tweetDownloader.py

--- tweetDownloader.py
+++ tweetDownloader.py
@@ -1,9 +1,9 @@
 import twitter
 
-api = twitter.Api(consumer_key="yo7TOsy8bdIu9ZlK7CdLnd0sN",
-	consumer_secret="sRUFYMk16y1f3jLEsfOfs2S0t0zHDC8vmcaAixH3UVawdXXYFR",
-	access_token_key="800131076622458881-36NJNS5xC5QIZH6BdB4bZIk0pRU902R",
-	access_token_secret="m7apKLNWbTcbHABFbgK40G1w6ysfWAmASPPtp62GqyGVF")
+api = twitter.Api(consumer_key="",
+	consumer_secret="",
+	access_token_key="",
+	access_token_secret="")
 
 statuses = api.GetUserTimeline('25073877', count=500)
 
